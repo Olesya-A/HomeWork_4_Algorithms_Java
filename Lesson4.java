7a6bcc0108dbd93e1aac11bf2c5f44409f06717d
Olesya <lesusha1@yandex.ru>
2023-08-18 18:45:21 +0300

    added lesson4.java

---
+++ Lesson4.java
@@ -0,0 +1,33 @@
+public class Lesson4 {
+    public static void main(String[] args) {
+        Tree<Integer> tree = new Tree<Integer>();
+        System.out.println(tree.add(8));
+        System.out.println(tree.add(1));
+        System.out.println(tree.add(3));
+        System.out.println(tree.add(12));
+        System.out.println(tree.add(12));
+
+        System.out.println();
+        System.out.println(tree.contains(8));
+        System.out.println(tree.contains(1));
+        System.out.println(tree.contains(3));
+        System.out.println(tree.contains(12));
+
+        System.out.println(tree.contains(4));
+        System.out.println(tree.contains(2));
+        System.out.println(tree.contains(14));
+
+        System.out.println(tree.findFirst());
+        System.out.println();
+        // tree.remove(8);
+        // tree.remove(12);
+        // tree.remove(12);
+        System.out.println(tree.contains(8));
+        System.out.println(tree.contains(1));
+        System.out.println(tree.contains(3));
+        System.out.println(tree.contains(12));
+        System.out.println(tree.dfs());
+        System.out.println(tree.bfs());
+
+    }
+}
